29a1dd45636fcc2ccbcc20690c69fa07d69d0caa
Adriano Sabo <acsabo@hotmail.com>
2015-05-07 21:54:26 -0300

    removing files too:

--- Ogaboo/Win32_Release/OgreTheoraDataStream.d
+++
@@ -1,4 +0,0 @@
-OgreTheoraDataStream.d: ../OgreTheoraDataStream.cpp \
- C:\Users\Adriano\git\Ogaboo\headers/OgreTheoraDataStream.h
-
-C:\Users\Adriano\git\Ogaboo\headers/OgreTheoraDataStream.h:
--- Ogaboo/Win32_Release/OpenAL_AudioInterface.d
+++
@@ -1,4 +0,0 @@
-OpenAL_AudioInterface.d: ../OpenAL_AudioInterface.cpp \
- C:\Users\Adriano\git\Ogaboo\headers/OpenAL_AudioInterface.h
-
-C:\Users\Adriano\git\Ogaboo\headers/OpenAL_AudioInterface.h:
